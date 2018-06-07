f806be111c3fca520b94f027b0c3092ae934351b
blue <937069176@qq.com>
2018-06-07 22:34:10 +0800

    前端测试界面增加

---
+++ app/test/controller/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blue
+ * Date: 2018/6/7
+ * Time: 17:29
+ */
+
+namespace app\test\controller;
+
+
+use cmf\controller\HomeBaseController;
+use helper\ArrayHelper;
+
+class IndexController extends HomeBaseController
+{
+    public function index(){
+        return $this->fetch(':index');
+    }
+
+    public function detailVideo(){
+        $param = $this->request->param();
+        $userId = ArrayHelper::getValue($param,'user_id');
+        $courseId = ArrayHelper::getValue($param,'course_id');
+        $this->assign('user_id',$userId);
+        $this->assign('course_id',$courseId);
+        return $this->fetch(':detail_video');
+    }
+}
